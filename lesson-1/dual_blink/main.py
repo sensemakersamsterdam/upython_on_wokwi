ab60c1fbb9e85169ddf2af8a1722ae2d4cd9f96e
Gijs Mos <gijs.mos@matanga.nl>
2024-10-11 22:24:22 +0200

    lesson 1, 2024-10-08

---
+++ lesson-1/dual_blink/main.py
@@ -0,0 +1,12 @@
+import time
+from machine import Pin
+
+RED_LED = Pin(19, Pin.OUT)
+RED_BLUE = Pin(18, Pin.OUT)
+
+while True:
+  current_value = RED_LED.value()
+  RED_LED.value( not current_value )
+  RED_BLUE.value( current_value )
+  time.sleep(0.25)
+
